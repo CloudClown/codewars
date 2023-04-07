56e0898a8e71f07f36a0e7178d091680f537afdf
tahmid <radon1441@gmail.com>
2023-04-07 22:09:39 +0600

    My head is at the wrong end!

---
+++ My-head-is-at-the-wrong-end.js
@@ -0,0 +1,9 @@
+function fixTheMeerkat(arr) {
+  [arr[0], arr[2]] = [arr[2], arr[0]];
+  return arr;
+//   return arr.reverse();
+
+}
+
+const result = fixTheMeerkat(["lower legs", "torso", "upper legs"]);
+console.log(result);
